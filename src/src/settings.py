9dd5e3cd11bacbf6bfab39adb10422763dafc40a
Liljack118 <kamilzajaczek4@gmail.com>
2022-12-15 23:01:37 +0100

    fix could not translate host name "db" in circleci

--- src/src/settings.py
+++ src/src/settings.py
@@ -109,6 +109,7 @@ DATABASES = {
         "USER": os.environ.get("DB_USER"),
         "PASSWORD": os.environ.get("DB_PASS"),
         "HOST": os.environ.get("DB_HOST"),
+        "PORT": os.environ.get("DATABASE_PORT"),
     }
 }
 
