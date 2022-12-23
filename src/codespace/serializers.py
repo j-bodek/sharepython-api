a663321cdf79c44b03f3a30fc32f51c496e0030b
Liljack118 <kamilzajaczek4@gmail.com>
2022-12-23 23:05:10 +0100

    create and use TmpCodeSpaceSerializer

--- src/codespace/serializers.py
+++ src/codespace/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-from core.models import CodeSpace
+from core.models import CodeSpace, TmpCodeSpace
+import uuid
 
 
 class CodeSpaceSerializer(serializers.ModelSerializer):
@@ -21,6 +22,19 @@ class CodeSpaceSerializer(serializers.ModelSerializer):
         )
 
 
-# @TODO
 class TmpCodeSpaceSerializer(serializers.Serializer):
-    pass
+    """
+    Temporary codespace is used to store only code without
+    any additional data, because it will be automatically deleted
+    from redis after time without updates defined in settings
+    """
+
+    uuid = serializers.UUIDField(default=lambda: f"tmp-{uuid.uuid4()}")
+    code = serializers.CharField(required=False)
+
+    def create(self, validated_data) -> dict:
+        # used to create new temporary
+        # codespace
+        tmp_codespace = TmpCodeSpace(**validated_data)
+        tmp_codespace.save()
+        return tmp_codespace
--- src/codespace/views.py
+++ src/codespace/views.py
@@ -1,8 +1,10 @@
 from rest_framework import generics
 from codespace.serializers import CodeSpaceSerializer, TmpCodeSpaceSerializer
 from rest_framework.response import Response
-from core.models import CodeSpace
+from core.models import CodeSpace, TmpCodeSpace
 from django.shortcuts import get_object_or_404
+from django.http import Http404
+from rest_framework import status
 
 
 class CreateCodeSpaceView(generics.CreateAPIView):
@@ -11,12 +13,21 @@ class CreateCodeSpaceView(generics.CreateAPIView):
     codespace_serializer_class = CodeSpaceSerializer
     tmp_codespace_serializer_class = TmpCodeSpaceSerializer
 
-    # @TODO add tmp_codespace_serializer
     def get_serializer_class(self):
+        # if user is not authenticated create temporary codespace
         if self.request.user.is_authenticated:
             return self.codespace_serializer_class
         else:
             return self.tmp_codespace_serializer_class
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(
+            serializer.data, status=status.HTTP_201_CREATED, headers=headers
+        )
 
     def perform_create(self, serializer):
         serializer.save(created_by=self.request.user)
@@ -25,12 +36,33 @@ class CreateCodeSpaceView(generics.CreateAPIView):
 class RetrieveCodeSpaceView(generics.RetrieveAPIView):
     """View used to retrieve codespace data."""
 
-    model_class = CodeSpace
-    serializer_class = CodeSpaceSerializer
+    codespace_class = CodeSpace
+    codespace_serializer_class = CodeSpaceSerializer
+
+    tmp_codespace_class = TmpCodeSpace
+    tmp_codespace_serializer_class = TmpCodeSpaceSerializer
+
+    def get_serializer_class(self):
+        """
+        Return either codespace or tmp codespace
+        serializer class
+        """
+
+        obj_uuid = self.kwargs.get("uuid", "")
+        if obj_uuid.startswith("tmp-"):
+            return self.tmp_codespace_serializer_class
+        else:
+            return self.codespace_serializer_class
 
     def get_object(self):
         obj_uuid = self.kwargs.get("uuid", "")
-        return get_object_or_404(CodeSpace, uuid=obj_uuid)
+        if obj_uuid.startswith("tmp-"):
+            try:
+                return TmpCodeSpace.objects.get(uuid=obj_uuid)
+            except TmpCodeSpace.DoesNotExist as e:
+                raise Http404(e)
+        else:
+            return get_object_or_404(CodeSpace, uuid=obj_uuid)
 
     def retrieve(self, request, *args, **kwargs):
         instance = self.get_object()
