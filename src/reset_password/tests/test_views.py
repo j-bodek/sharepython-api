cad00501bde6c8c23dc5963494eed46388bb3b06
Liljack118 <kamilzajaczek4@gmail.com>
2023-02-13 21:59:41 +0100

    add tests for ValidateResetPasswordView

--- src/reset_password/tests/test_views.py
+++ src/reset_password/tests/test_views.py
@@ -1,5 +1,6 @@
 from django.test import SimpleTestCase
 from rest_framework.test import APIClient
+from rest_framework import serializers
 from django.urls import reverse
 from unittest import mock
 from reset_password import views as reset_pwd_views
@@ -55,3 +56,41 @@ class TestRequestResetPasswordView(SimpleTestCase):
         mocked_request_password_reset_signal.send.assert_called_once_with(
             sender=self.view_class, serializer=mocked_serializer
         )
+
+
+class TestValidateResetPasswordView(SimpleTestCase):
+    """Test ValidateResetPasswordView class"""
+
+    def setUp(self):
+        self.client = APIClient()
+
+    @mock.patch("reset_password.views.ValidateResetPasswordView.get_serializer")
+    def test_with_invalid_token(self, mocked_get_serializer):
+        """Test if response with status 400 is returned"""
+
+        mocked_serializer = mock.MagicMock()
+        mocked_serializer.is_valid.side_effect = [serializers.ValidationError]
+        mocked_get_serializer.return_value = mocked_serializer
+
+        data = {"email": "test@example.com", "token": "invalid_token"}
+        r = self.client.post(
+            reverse("reset_password:reset_password_validate"),
+            data=data,
+        )
+
+        self.assertEqual(r.status_code, 400)
+
+    @mock.patch("reset_password.views.ValidateResetPasswordView.get_serializer")
+    def test_with_valid_token(self, mocked_get_serializer):
+        """Test if response with status 200 is returned"""
+
+        data = {"email": "test@example.com", "token": "valid_token"}
+        mocked_serializer = mock.MagicMock(data=data)
+        mocked_get_serializer.return_value = mocked_serializer
+
+        r = self.client.post(
+            reverse("reset_password:reset_password_validate"),
+            data=data,
+        )
+
+        self.assertEqual(r.status_code, 200)
